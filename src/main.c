361e2e6381eaa1916ef6621bd6e0f4097718295c
Tituya <tituya02@gmail.com>
2020-06-29 00:08:48 +0200

    Ajout touche unique gravité

--- src/main.c
+++ src/main.c
@@ -12,12 +12,25 @@ int main(void)
 	int player_x=30, player_y=35;
 	char level[351];
 	char gravity = 0; //0 down 1 up
+	char check = 0;
 	int id_level = 1;
 	set_level(id_level,level);
 	draw_level(level);
 	while(1)
 	{
+		draw_level(level);
+		draw_player(player_x,player_y);
+		
+		dprint(150,100,C_GREEN,"%d",player_x);
+		dprint(150,120,C_GREEN,"%d",player_y);
+
+		dprint(300,120,C_GREEN,"%c",level[(int)((player_x-1)/16)+(int)((player_y-1)/16*25)]); //top left
+		dprint(300,140,C_GREEN,"%c",level[(int)((player_x+PLAYER_HEIGHT)/16)+(int)((player_y-1)/16*25)]); //top right
+		dprint(300,160,C_GREEN,"%c",level[(int)(player_x/16)+(int)((player_y+PLAYER_HEIGHT)/16*25)]); //bottom left
+		dprint(300,180,C_GREEN,"%c",level[(int)((player_x+PLAYER_HEIGHT)/16)+(int)((player_y+PLAYER_HEIGHT)/16*25)]); //bottom right
+		
 		dupdate();
+		
 		pollevent();
 		if(keydown(KEY_RIGHT) && collide_hor(player_x, player_y, level)!=1)
 		{
@@ -28,14 +41,19 @@ int main(void)
 		else if(keydown(KEY_LEFT) && collide_hor(player_x, player_y, level)!=2)
 		{
 			player_x-=PLAYER_SPEED;
-			if(player_x<-9) player_x=386;
+			if(player_x<-9) player_x=384;
 		}
+		if(keydown(KEY_SHIFT) && !check && collide_vert(player_x, player_y, level, gravity))
+		{
+			if(!gravity) gravity=1;
+			else gravity=0;
+			check=1;
+		}
+		else if(!keydown(KEY_SHIFT) && check) check=0;
 		if(keydown(KEY_EXIT))
 		{
 			break;
 		}
-		if(keydown(KEY_OPTN)) gravity=1;
-		else if(keydown(KEY_VARS)) gravity=0;
 		switch (collide_vert(player_x, player_y, level, gravity))
 		{
 			case 0:
@@ -49,16 +67,6 @@ int main(void)
 				else if((player_y)%16!=0 && gravity) player_y++;
 				break;
 		}
-		draw_level(level);
-		draw_player(player_x,player_y);
-
-		dprint(150,100,C_GREEN,"%d",player_x);
-		dprint(150,150,C_GREEN,"%d",player_y);
-
-		dprint(200,120,C_GREEN,"%c",level[(int)((player_x-1)/16)+(int)((player_y-1)/16*25)]); //top left
-		dprint(200,140,C_GREEN,"%c",level[(int)((player_x+12)/16)+(int)((player_y-1)/16*25)]); //top right
-		dprint(200,160,C_GREEN,"%c",level[(int)(player_x/16)+(int)((player_y+12)/16*25)]); //bottom left
-		dprint(200,180,C_GREEN,"%c",level[(int)((player_x+12)/16)+(int)((player_y+12)/16*25)]); //bottom right
 	}
 	return 0;
 }
