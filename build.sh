3cf39462de5223e5255927615eb97e22b0a13c75
Tituya <tituya02@gmail.com>
2020-11-14 17:59:59 +0100

    Patch editeur en python3

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 echo "making setlevel.c from editor"
 rm src/setlevel.c
-python compile_level.py
+python3 compile_level.py
 echo "done"
 echo "making times.c from times.lvl"
 rm src/times.c
-python time.py
+python3 time.py
 echo "done"
 echo "building for cg"
 fxsdk build-cg
